716ac35fd6af70d01c0966ec6ebf3e7626f45603
Ale <alespa20075@gmail.com>
2025-02-16 02:29:34 +0100

    aggiunto metodo loadImage anche in classe Player

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -10,5 +10,6 @@ public class Entity {
     public int spriteNum = 1;
 
     public String direction;
-    public BufferedImage su1, su2, giu1, giu2, destra1, destra2, sinistra1, sinistra2, fermoSu, fermoGiu, fermoDestra, fermoSinistra;
+    public BufferedImage su1, su2, giu1, giu2, destra1, destra2, sinistra1, sinistra2, fermoSu, fermoGiu, fermoDestra,
+            fermoSinistra;
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -9,32 +9,34 @@ import Gioco.GamePanel;
 import Gioco.GestioneTasti;
 import utils.Defines;
 
-public class Player extends Entity{
-        GamePanel panGioco;
-        GestioneTasti gestioneTasti;
+public class Player extends Entity {
+    GamePanel panGioco;
+    GestioneTasti gestioneTasti;
 
-        public final int screenX;
-        public final int screenY;
+    public final int screenX;
+    public final int screenY;
 
-
-    public Player(GamePanel g1, GestioneTasti g2){
+    public Player(GamePanel g1, GestioneTasti g2) {
         this.panGioco = g1;
         gestioneTasti = g2;
 
-        screenX = Defines.SCREEN_WIDTH/2 - Defines.GRANDEZZA_CASELLE;
-        screenY = Defines.SCREEN_HEIGHT/2 - Defines.GRANDEZZA_CASELLE;
+        screenX = Defines.SCREEN_WIDTH / 2 - Defines.GRANDEZZA_CASELLE;
+        screenY = Defines.SCREEN_HEIGHT / 2 - Defines.GRANDEZZA_CASELLE;
 
         setDefaultValues();
     }
-    public void setDefaultValues(){
-        worldX = Defines.GRANDEZZA_CASELLE*8;
-        worldY = Defines.GRANDEZZA_CASELLE*5; 
+
+    public void setDefaultValues() {
+        worldX = Defines.GRANDEZZA_CASELLE * 8;
+        worldY = Defines.GRANDEZZA_CASELLE * 5;
         speed = 4;
         direction = "su";
         getPlayerImage();
     }
-    public void update(){
-        if (gestioneTasti.suPremuto || gestioneTasti.giuPremuto || gestioneTasti.destraPremuto || gestioneTasti.sinistraPremuto) {
+
+    public void update() {
+        if (gestioneTasti.suPremuto || gestioneTasti.giuPremuto || gestioneTasti.destraPremuto
+                || gestioneTasti.sinistraPremuto) {
             if (gestioneTasti.suPremuto) {
                 direction = "su";
                 worldY -= speed;
@@ -54,22 +56,24 @@ public class Player extends Entity{
             spriteCounter++;
             if (spriteCounter > 12) {
                 if (spriteNum == 1) {
-                    spriteNum=2;
-                }else if(spriteNum == 2){
-                    spriteNum=1;
+                    spriteNum = 2;
+                } else if (spriteNum == 2) {
+                    spriteNum = 1;
                 }
-                spriteCounter=0;
+                spriteCounter = 0;
             }
-        }else{
+        } else {
             switch (direction) {
                 case "giu":
-            
+
                     direction = "fermoGiu";
                     spriteCounter++;
                     if (spriteCounter > 24) {
-                        if (spriteNum == 1) spriteNum=2;
-                        else if(spriteNum == 2) spriteNum=1;
-                        spriteCounter=0;
+                        if (spriteNum == 1)
+                            spriteNum = 2;
+                        else if (spriteNum == 2)
+                            spriteNum = 1;
+                        spriteCounter = 0;
                     }
 
                     break;
@@ -77,27 +81,33 @@ public class Player extends Entity{
                     direction = "fermoSu";
                     spriteCounter++;
                     if (spriteCounter > 24) {
-                        if (spriteNum == 1) spriteNum=2;
-                        else if(spriteNum == 2) spriteNum=1;
-                        spriteCounter=0;
+                        if (spriteNum == 1)
+                            spriteNum = 2;
+                        else if (spriteNum == 2)
+                            spriteNum = 1;
+                        spriteCounter = 0;
                     }
                     break;
                 case "destra":
                     direction = "fermoGiu";
                     spriteCounter++;
                     if (spriteCounter > 24) {
-                        if (spriteNum == 1) spriteNum=2;
-                        else if(spriteNum == 2) spriteNum=1;
-                        spriteCounter=0;
+                        if (spriteNum == 1)
+                            spriteNum = 2;
+                        else if (spriteNum == 2)
+                            spriteNum = 1;
+                        spriteCounter = 0;
                     }
                     break;
                 case "sinistra":
                     direction = "fermoGiu";
                     spriteCounter++;
                     if (spriteCounter > 24) {
-                        if (spriteNum == 1) spriteNum=2;
-                        else if(spriteNum == 2) spriteNum=1;
-                        spriteCounter=0;
+                        if (spriteNum == 1)
+                            spriteNum = 2;
+                        else if (spriteNum == 2)
+                            spriteNum = 1;
+                        spriteCounter = 0;
                     }
                     break;
                 default:
@@ -105,102 +115,71 @@ public class Player extends Entity{
             }
         }
     }
-    public void getPlayerImage(){
+
+    public void getPlayerImage() {
+        fermoSu = loadImage("/res/player/playerFermoSu.png");
+
+        fermoGiu = loadImage("/res/player/playerFermoGiu.png");
+
+        su1 = loadImage("/res/player/player_up_1.png");
+
+        su2 = loadImage("/res/player/player_up_2.png");
+
+        giu1 = loadImage("/res/player/player_down_1.png");
+
+        giu2 = loadImage("/res/player/player_down_2.png");
+
+        destra1 = loadImage("/res/player/player_right_1.png");
+
+        destra2 = loadImage("/res/player/player_right_2.png");
+
+        sinistra1 = loadImage("/res/player/player_left_1.png");
+
+        sinistra2 = loadImage("/res/player/player_left_2.png");
+    }
+
+    private BufferedImage loadImage(String percorso) {
         try {
-            fermoSu = ImageIO.read(getClass().getClassLoader().getResourceAsStream("res/player/playerFermoSu.png"));
+            return ImageIO.read(getClass().getResourceAsStream(percorso));
         } catch (Exception e) {
-            System.err.println("erorre caricamento fermoSu");
-        }
-
-
-        try{
-            fermoGiu = ImageIO.read(getClass().getClassLoader().getResourceAsStream("res/player/playerFermoGiu.png"));
-        } catch (Exception e) {
-            System.err.println("Errore caricamento fermoGiu");
-        }
-
-
-        try{
-            su1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("res/player/player_up_1.png"));
-        } catch (Exception e) {
-            System.err.println("Errore caricamento su1");
-        }
-
-
-        try{
-            su2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("res/player/player_up_2.png"));
-        } catch (Exception e) {
-            System.err.println("Errore caricamento su2");
-        }
-
-
-        try{
-            giu1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("res/player/player_down_1.png"));
-        } catch (Exception e) {
-            System.err.println("Errore caricamento giu1");
-        }
-
-
-        try{
-            giu2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("res/player/player_down_2.png"));
-        } catch (Exception e) {
-            System.err.println("Errore caricamento giu2");
-        }
-
-
-        try{
-            destra1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("res/player/player_right_1.png"));
-        } catch (Exception e) {
-            System.err.println("Errore caricamento destra1");
-        }
-
-
-        try{
-            destra2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("res/player/player_right_2.png"));
-        } catch (Exception e) {
-            System.err.println("Errore caricamento destra2");
-        }
-
-
-        try{
-            sinistra1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("res/player/player_left_1.png"));
-        } catch (Exception e) {
-            System.err.println("Errore caricamento sinistra1");
-        }
-
-        
-        try{
-            sinistra2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("res/player/player_left_2.png"));
-        } catch (Exception e) {
-            System.err.println("Errore caricamento sinistra2");
+            System.err.println("Immagine non trovata in: " + percorso);
+            return null;
         }
     }
 
-    public void draw(Graphics2D g){
+    public void draw(Graphics2D g) {
         BufferedImage image = null;
         switch (direction) {
             case "su":
-                
-                if (spriteNum==1) image = su1;
-                else image = su2;
+
+                if (spriteNum == 1)
+                    image = su1;
+                else
+                    image = su2;
 
                 break;
             case "giu":
 
-                if (spriteNum==1) image = giu1;
-                else image = giu2;
+                if (spriteNum == 1)
+                    image = giu1;
+                else
+                    image = giu2;
 
                 break;
             case "destra":
 
-                if (spriteNum==1) image = destra1;
-                else image = destra2;
+                if (spriteNum == 1)
+                    image = destra1;
+                else
+                    image = destra2;
 
                 break;
             case "sinistra":
 
-                if (spriteNum==1) image = sinistra1;
-                else image = sinistra2;  
+                if (spriteNum == 1)
+                    image = sinistra1;
+                else
+                    image = sinistra2;
 
                 break;
             case "fermoSu":
