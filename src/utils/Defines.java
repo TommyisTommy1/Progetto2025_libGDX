8c56b8585ac242564f1d013ecb73025e5bf3f218
Ale <alespa20075@gmail.com>
2025-02-17 16:20:17 +0100

    pt2

--- src/utils/Defines.java
+++ src/utils/Defines.java
@@ -11,19 +11,8 @@ import tile.TileManager;
 import mp3PlayerPkg.MP3Player;
 
 public class Defines {
-
-    public static final Toolkit toolkit = Toolkit.getDefaultToolkit();
-    // MP3 AUDIO CONTROL
-    public static final MP3Player AUDIO_PLAYER = new MP3Player();
-    public static final Thread MP3_PLAYER = new Thread(AUDIO_PLAYER);
-    // VARIABILI SCHERMO
     public static final int SCREEN_WIDTH = 854; // (int)toolkit.getScreenSize().getWidth();
     public static final int SCREEN_HEIGHT = 480; // (int)toolkit.getScreenSize().getHeight();
-    public static final JFrame MAINFRAME = new JFrame("GameScreen");
-
-    // VARIABILI PANNELLI (DA SPIEGARE SPARTIX)
-    public static final GamePanel GAME_PANEL = new GamePanel();
-    public static final JPanel CONTENT_PANEL = new JPanel(new BorderLayout());
 
     public static int NUM_COLONNE = 50; // modificabile per cambiare la dimensione della visuale
     public static int NUM_RIGHE = 50; // si modifica a seconda della prima per avere la risuluzione in 16:9
@@ -35,6 +24,22 @@ public class Defines {
 
     public static final int GRANDEZZA_CASELLE = (int) Math .ceil(GRANDEZZA_CASELLE_ORIGINALE * SCALA / 0.8); //L'ultimo numero è il moltiplicatore della telecamera
 
+
+    public static final Toolkit toolkit = Toolkit.getDefaultToolkit();
+    // MP3 AUDIO CONTROL
+    public static final MP3Player AUDIO_PLAYER = new MP3Player();
+    public static final Thread MP3_PLAYER = new Thread(AUDIO_PLAYER);
+    // VARIABILI SCHERMO
+    
+    public static final JFrame MAINFRAME = new JFrame("GameScreen");
+
+    // VARIABILI PANNELLI (DA SPIEGARE SPARTIX)
+    public static final GamePanel GAME_PANEL = new GamePanel();
+    public static final JPanel CONTENT_PANEL = new JPanel(new BorderLayout());
+
+    
+
+    
     public static final int FPS = 60;
 
     public static final Thread THREAD_GIOCO = new Thread(GAME_PANEL);
